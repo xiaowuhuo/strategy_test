8469a87db0eef232e3416583ff7d5d67554dc42a
gordon <a731450465@qq.com>
2017-07-20 20:27:05 +0800

    add url

--- Context.php
+++ Context.php
@@ -4,6 +4,9 @@
  * User: gordon
  * Date: 2017/7/20
  * Time: 下午8:04
+ *
+ * http://www.runoob.com/design-pattern/strategy-pattern.html
+ *
  */
 class Context implements work {
     private $strategy;
@@ -13,7 +16,7 @@ class Context implements work {
         $flow = $this->strategy->getConf(get_class($this->strategy));
         $current = $this->strategy->getCurrent();
         $history = $this->strategy->getHistory();
-        
+
 
         return [];
     }
